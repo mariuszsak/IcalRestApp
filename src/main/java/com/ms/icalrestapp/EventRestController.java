6e0189a1941eae2c3f2763b9a875bfc571adbe91
mariuszsak <mariuszsak@gmail.com>
2019-06-06 14:26:12 +0200

    added method for pass event to database

--- src/main/java/com/ms/icalrestapp/EventRestController.java
+++ src/main/java/com/ms/icalrestapp/EventRestController.java
@@ -4,12 +4,15 @@ import com.ms.icalrestapp.model.EventModel;
 import com.ms.icalrestapp.repository.EventRepository;
 import com.ms.icalrestapp.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
+@CrossOrigin
 @RestController
 public class EventRestController {
 
@@ -23,10 +26,10 @@ public class EventRestController {
     }
 
 
-    @RequestMapping(value = "/pass", consumes = "application/json", method = {RequestMethod.GET, RequestMethod.PUT})
-    public void getEvent(@RequestBody EventModel eventModel) {
+    @RequestMapping(value = "/pass", consumes = "application/json", method = {RequestMethod.GET, RequestMethod.PUT, RequestMethod.POST})
+    public ResponseEntity<EventModel> getEvent(@RequestBody EventModel eventModel) {
         eventRepository.save(eventModel);
-
+        return ResponseEntity.status(HttpStatus.OK).body(eventModel);
     }
 
 
